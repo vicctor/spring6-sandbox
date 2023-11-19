eb7395e5e52776e552d6b5305cb4840f0563fa7b
vicctor <artur.keska@gmail.com>
2023-11-19 20:43:51 +0100

    fix for flux or operator

--- spring6-security/src/main/java/com/example/demo/WebFluxSecurityApplication.java
+++ spring6-security/src/main/java/com/example/demo/WebFluxSecurityApplication.java
@@ -31,86 +31,87 @@ import java.util.stream.Collectors;
 @RestController
 @EnableReactiveMethodSecurity
 public class WebFluxSecurityApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(WebFluxSecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxSecurityApplication.class, args);
+    }
 
-	@Bean
-	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-		http
-				.authorizeExchange(ex ->
-						ex
-								.pathMatchers("/one").permitAll()
-								.pathMatchers("/two").hasAnyAuthority("authority_1")
-								.pathMatchers("/three/*").access(endingWith3AllowedOnly)
-								.pathMatchers("/three/or/six/*").access(or(endingWith3AllowedOnly, endingWith6AllowedOnly))
-								.pathMatchers("/even/*").hasAnyAuthority("no_odd")
-				)
-				.authenticationManager(reactiveAuthenticationManager)
-				.httpBasic(Customizer.withDefaults());
-		return http.build();
-	}
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(ex -> ex
+                                .pathMatchers("/one").permitAll()
+                                .pathMatchers("/two").hasAnyAuthority("authority_1")
+                                .pathMatchers("/three/*").access(endingWith3AllowedOnly)
+                                .pathMatchers("/three/or/six/*").access(or(endingWith3AllowedOnly, endingWith6AllowedOnly))
+                                .pathMatchers("/even/*").hasAnyAuthority("no_odd")
+                )
+                .authenticationManager(reactiveAuthenticationManager)
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 
-	private <T> ReactiveAuthorizationManager<T> or(ReactiveAuthorizationManager<T>... managers) {
-		return new ReactiveAuthorizationManager<T>() {
-			@Override
-			public Mono<AuthorizationDecision> check(Mono<Authentication> authentication, T context) {
-				var decision = Flux.fromArray(managers)
-						.map(a -> check(authentication, context))
-						.reduce((p, q) -> p.or(q))
-						.flatMap(p -> p);
-				return decision;
-			}
-		};
-	}
+    private <T> ReactiveAuthorizationManager<T> or(ReactiveAuthorizationManager<T>... managers) {
+        return (authentication, context) ->
+                Flux.merge(
+                    Arrays.stream(managers)
+                          .map(m -> m.check(authentication, context))
+                          .collect(Collectors.toList())
+						  )
+                .reduce((a, b) -> new AuthorizationDecision(a.isGranted() || b.isGranted()));
+    }
 
-	private ReactiveAuthenticationManager reactiveAuthenticationManager = authentication -> Mono.just(
-				switch (authentication) {
-					case UsernamePasswordAuthenticationToken a -> getUsernamePasswordAuthenticationToken(a);
-					default -> throw new IllegalStateException("Unexpected value: " + authentication);
-				}
-		);
+    private ReactiveAuthenticationManager reactiveAuthenticationManager = authentication -> Mono.just(
+                switch (authentication) {
+                    case UsernamePasswordAuthenticationToken a -> getUsernamePasswordAuthenticationToken(a);
+                    default -> throw new IllegalStateException("Unexpected value: " + authentication);
+                }
+        );
 
-	private ReactiveAuthorizationManager<AuthorizationContext> endingWith3AllowedOnly = endingWithAuthorizationConstraint(3);
-	private ReactiveAuthorizationManager<AuthorizationContext> endingWith6AllowedOnly = endingWithAuthorizationConstraint(6);
+    private ReactiveAuthorizationManager<AuthorizationContext> endingWith3AllowedOnly = endingWithAuthorizationConstraint(3);
+    private ReactiveAuthorizationManager<AuthorizationContext> endingWith6AllowedOnly = endingWithAuthorizationConstraint(6);
 
-	private ReactiveAuthorizationManager<AuthorizationContext> endingWithAuthorizationConstraint(int with) {
-		return (authentication, object) -> authentication.map(a -> new AuthorizationDecision(
-				object.getExchange().getRequest().getPath().toString().endsWith("/" + with)
-		));
-	}
+    private ReactiveAuthorizationManager<AuthorizationContext> endingWithAuthorizationConstraint(int with) {
+        return (authentication, object) -> authentication.map(a -> new AuthorizationDecision(
+                object.getExchange().getRequest().getPath().toString().endsWith("/" + with)
+        ));
+    }
 
-	private UsernamePasswordAuthenticationToken getUsernamePasswordAuthenticationToken(UsernamePasswordAuthenticationToken a) {
-		var password = a.getCredentials().toString().split(",");
-		var authorities = Arrays.stream(password).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
-		return new UsernamePasswordAuthenticationToken(a, a.getPrincipal(), authorities);
-	}
+    private UsernamePasswordAuthenticationToken getUsernamePasswordAuthenticationToken(UsernamePasswordAuthenticationToken a) {
+        var password = a.getCredentials().toString().split(",");
+        var authorities = Arrays.stream(password).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+        return new UsernamePasswordAuthenticationToken(a, a.getPrincipal(), authorities);
+    }
 
-	@Component("authExpressions")
-	public static class WebSecurity {
-		public boolean checkIsEven(int value) {
-				return value % 2 == 0;
-		}
-	}
+    @Component("authExpressions")
+    public static class WebSecurity {
+        public boolean checkIsEven(int value) {
+                return value % 2 == 0;
+        }
+    }
 
-	@GetMapping("one")
-	public int one() {
-		return 1;
-	}
+    @GetMapping("one")
+    public int one() {
+        return 1;
+    }
 
-	@GetMapping("two")
-	public int two() {
-		return 2;
-	}
+    @GetMapping("two")
+    public int two() {
+        return 2;
+    }
 
-	@GetMapping("three/{value}")
-	public int three(@PathVariable("value") int value) {
-		return value;
-	}
+    @GetMapping("three/{value}")
+    public int three(@PathVariable("value") int value) {
+        return value;
+    }
 
-	@GetMapping("even/{value}")
-	@PreAuthorize("@authExpressions.checkIsEven(#value)")
-	public Mono<Integer> even(@PathVariable("value") int value) {
-		return Mono.just(value);
-	}
+    @GetMapping("three/or/six/{value}")
+    public int threeOrSix(@PathVariable("value") int value) {
+        return value;
+    }
+
+    @GetMapping("even/{value}")
+    @PreAuthorize("@authExpressions.checkIsEven(#value)")
+    public Mono<Integer> even(@PathVariable("value") int value) {
+        return Mono.just(value);
+    }
 }
